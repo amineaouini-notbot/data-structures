f826953b7f7fbae7aac07ddd96255d1c16431d5f
amine m. aouini <aminaouini565@gmail.com>
2023-11-05 14:40:26 +0100

    get stack length

--- stack.py
+++ stack.py
@@ -5,10 +5,13 @@ class stackExample:
 
     def push(self, value):
         self.data.append(value)
-
+    def length(self):
+        return len(self.data)
     
 stack=stackExample()
 
 stack.push(1)
+stack.push(3)
 
 print(stack.data)
+print(stack.length())
