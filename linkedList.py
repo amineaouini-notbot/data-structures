b532b8c116ac22b344484c83f943a6a5aace947c
amine m. aouini <aminaouini565@gmail.com>
2023-11-10 15:30:49 +0100

    remove list node by valur

--- linkedList.py
+++ linkedList.py
@@ -19,11 +19,26 @@ class linkedList:
             print('value not found!')
             return 'value not found!'
 
+    def rmNode(self, value):
+        if(self.val == value):
+            self.val = self.next.val
+            self.next = self.next.next
+            print('node removed!!')
+            return 
+        elif (self.next == None):
+            print('node not found to be deleted!!')
+            return
+        else: 
+            return self.next.rmNode(value)
+
 list = linkedList(1) # 1 => x
 
 list.add(2) # 1 => 2 => x
 
 list.add(3) # 1 => 2 => 3 => x 
-
+list.add(5) # 1 => 2 => 3 => 5 => x
 list.find(4) # should value be not found
 list.find(2) # value should be found 
+list.rmNode(2) # 1 => 3 => 5 => x
+list.find(2)
+print(list.val, list.next.val, list.next.next.val)
